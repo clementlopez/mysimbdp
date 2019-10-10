0f51a0a6fefe137b372aec5603c03b5101e7dba8
clement.lopez <clement.lopez@moonshotlabs.com>
2019-10-10 10:04:35 +0300

    multi-threads insertions + plot time results

--- code/mysimbdp_collect_data_csv.py
+++ code/mysimbdp_collect_data_csv.py
@@ -5,27 +5,29 @@ from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 import argparse
 import pandas as pd
+from timeit import default_timer as d_timer
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Init authentication')
     parser.add_argument('--user', type=str, help='You must fill in a String', default='cassandra')
     parser.add_argument('--password', type=str, help='You must fill in a String',  default='cassandra')
     parser.add_argument('--port', type=int, help='You must fill in a Int',  default=9042)
-    parser.add_argument('--adress', type=str, help='You must fill in a Int',  default='0.0.0.0')
+    parser.add_argument('--adress', type=str, help='You must fill in a String',  default='0.0.0.0')
+    parser.add_argument('--id', type=str, help='You must fill in a Int',  default=0)
     return parser.parse_args()
 
 def fill_database(session):
-    #df = pd.read_csv('../data/cassandra/cleanGoogleplaystore.csv', dtype)
     with open('../data/cassandra/cleanGoogleplaystore.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
+        timer = [['0','0']]
+        debut = d_timer()
         for row in csv_reader:
             if line_count > 0:
                 rating = float(row[2])
                 rev = int(row[3])
                 fr = bool(row[6])
                 price = float(row[7])
-                #last_up = datetime.strptime(row[10], "%B %d, %Y").date()
                 session.execute("""
                 INSERT INTO Application (id, name, category, rating, reviews, size, installs, free, price_dollar, content_rating, genres, last_update, current_ver, android_ver)
                 values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
@@ -35,6 +37,9 @@ def fill_database(session):
                 row[9], row[10], row[11], row[12])
                 )
             line_count += 1
+            if line_count%1000==0:
+                timer.append([str(line_count),str(d_timer()-debut)])
+    return timer
 
 
 
@@ -50,7 +55,18 @@ if __name__ == "__main__":
     #Enter in the keyspace
     session.execute("USE MyKeyspace;")
 
-    fill_database(session)
+    result = fill_database(session)
+
+    headers = ['NbValues','Time']
+
+    f = open('../data/cassandra/Result'+args.id+'.csv', 'w')
+    headersLine = ",".join(headers) + "\n"
+    f.write(headersLine)
+    for val in result:
+        line = ",".join(val) + "\n"
+        f.write(line)
+
+    f.close()
 
     #Close cluster
     cluster.shutdown()
---
+++ code/mysimbdp_multi_threads.py
@@ -0,0 +1,35 @@
+from multiprocessing import Pool, TimeoutError, Process
+import time
+from timeit import default_timer as d_timer
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+def run_process(process):                                                             
+    os.system('python {}'.format(process))
+
+if __name__ == '__main__':
+    #Multi-threads
+    debut = d_timer()
+    nbProcess = 10
+    processes = []
+    for i in range(nbProcess):
+        processes.append('mysimbdp_collect_data_csv.py --id '+str(i))
+    pool = Pool(processes=nbProcess)
+    pool.map(run_process, processes)
+
+    #data display
+    for i in range(nbProcess):
+        with open('../data/cassandra/Result'+str(i)+'.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            timeCount = []
+            numberInsert = []
+            for row in csv_reader:
+                if line_count > 0:
+                    numberInsert.append(int(row[0]))
+                    timeCount.append(float(row[1]))
+                line_count+=1
+            plt.plot(timeCount, numberInsert)
+    plt.show()
